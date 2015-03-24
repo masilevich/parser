a4c8bbc03e50c5a6ce0a64f709e8844471be07ca
Masilevich Leanid <l.masilevich@gmail.com>
2015-03-24 17:15:09 +0300

    first parsing test

--- bin/injapan_parser.rb
+++ bin/injapan_parser.rb
@@ -1,57 +1,67 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'net/smtp'
+require 'open-uri'
+require 'nokogiri'
 
 SEND_TO = 'l.masilevich@gmail.com'
 
-def ping_api_1
-  post_check('http://example.com/resources')
-end
+def parse(query) 
 
-def get_check(url) 
-  begin
-    res = Net::HTTP.get_response(URI(url))
-    if res.code == "200"
-      puts "alive #{url}"
-    else
-      send_message("Something wrong with #{url}")
-    end
-  rescue => e
-    send_message("Something wrong with #{url} #{e.inspect}")
+  results_array_file = "#{query}.txt"
+
+  previous_elements =[]
+
+  results_file = File.open(results_array_file, "a+")
+  results_file.each_line do |line|
+    previous_elements << line.chomp
   end
+
+  url = "https://injapan.ru/search/do.html?query=#{query}"
+  html = open(url)
+  doc = Nokogiri::HTML(html)
+  
+  current_elements = []
+  doc.xpath('//div[@id="content"]/div/div/div/div/a/@href').each do |node|
+    current_elements << "https://injapan.ru#{node.text}"
+  end
+  current_elements.uniq!
+
+  new_elements = current_elements - previous_elements
+
+  send_message("Появились новые результаты для запроса #{query}", new_elements)
+
+  new_elements.each { |element| results_file.puts(element) }
+  results_file.close
+
 end
 
-def full_message(msg)
-  <<MESSAGE_END
-  From: mlvnotifier <mlvnotifier@gmail.com>
-  To: Developers
-  MIME-Version: 1.0
-  Content-type: text/html
-  Subject: #{msg}
-
-  <h2>Error</h2>
-  <br/>
-  #{msg}<br/>
-  <br/>
-
-  MESSAGE_END
+def full_message(subject, msg)
+<<MESSAGE_END
+From: mlvnotifier <mlvnotifier@gmail.com>
+To: Developers
+MIME-Version: 1.0
+Content-type: text/html
+Subject: subject
+ 
+<br/>
+#{msg}<br/>
+<br/>
+ 
+MESSAGE_END
 end
 
-def send_message(msg)
+def send_message(subject, msg)
   smtp = Net::SMTP.new 'smtp.gmail.com', 587
   smtp.enable_starttls
   smtp.start("gmail.com", "mlvnotifier@gmail.com", '5Lipp7BDIk', :login) do |smtp|
-    smtp.send_message(full_message(msg), "mlvnotifier@gmail.com", SEND_TO)
+    smtp.send_message(full_message(subject, msg), "mlvnotifier@gmail.com", SEND_TO)
   end
 end
 
 puts "Start scanning at #{Time.now}"
 
-threads = []
-threads << Thread.new { parse('ステラ+C2000S') }
-threads << Thread.new { parse('ステラ+1000S') }
+parse('stella+C2000S')
+parse('stella+1000S')
 
-threads.each do |t|
-  t.join
-end
 puts "Complete scanning at #{Time.now}"
