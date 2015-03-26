f12816029591079c66e77ac3e9d330ac0fb00b9d
Masilevich Leanid <l.masilevich@gmail.com>
2015-03-26 12:49:09 +0300

    asdf

--- tmp.rb
+++ tmp.rb
@@ -1,20 +1,8 @@
-  file_name = "./tmp/out.txt"
-  if File.exist?(file_name)
-    out_file = File.open(file_name, "r")
-    puts "Before writing File exist File content: #{out_file.read}"
-    out_file.close
-  else
-    puts "Before writing File not exist"
-  end
+file_name = "./tmp/out.txt"
 
-  out_file = File.new(file_name, "a")
-  out_file.puts("write your stuff here")
-  out_file.close
+out_file = File.open(file_name, "a+")
+puts "Before writing File content: #{out_file.read}"
 
-  if File.exist?(file_name)
-    out_file = File.open(file_name, "r")
-    puts "After writing File exist File content: #{out_file.read}"
-    out_file.close
-  else
-    puts "After writing File not exist"
-  end
+out_file.puts("write your stuff here")
+
+out_file.close
