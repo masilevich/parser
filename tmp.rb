533389489dafd3ad10d86da8f2fa9f95f4a00e26
Masilevich Leanid <l.masilevich@gmail.com>
2015-03-26 11:56:43 +0300

    tmp

--- tmp.rb
+++ tmp.rb
@@ -1,4 +1,4 @@
-  if File.exist?("tmp/out.txt")
+  if File.exist?("./tmp/out.txt")
     out_file = File.open("tmp/out.txt", "r")
     puts "Before writing File exist File content: #{out_file.read}"
     out_file.close
@@ -6,11 +6,11 @@
     puts "Before writing File not exist"
   end
 
-  out_file = File.new("tmp/out.txt", "a")
+  out_file = File.new("./tmp/out.txt", "a")
   out_file.puts("write your stuff here")
   out_file.close
 
-  if File.exist?("tmp/out.txt")
+  if File.exist?("./tmp/out.txt")
     out_file = File.open("tmp/out.txt", "r")
     puts "After writing File exist File content: #{out_file.read}"
     out_file.close
