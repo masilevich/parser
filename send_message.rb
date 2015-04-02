edf57e0c061751b3f27360e4c0fa8e396f488bdd
Masilevich Leanid <l.masilevich@gmail.com>
2015-04-02 12:15:32 +0300

    asdf

--- send_message.rb
+++ send_message.rb
@@ -7,7 +7,7 @@ DEFAULT_SEND_TO = 'l.masilevich@gmail.com'
 
 def full_message(to, subject, msg)
 <<MESSAGE_END
-From: mlvnotifier <mlvnotifier@gmail.com>
+From: <#{ENV['MAIL_LOGIN']}>
 To: #{to}
 MIME-Version: 1.0
 Content-type: text/html
@@ -22,7 +22,7 @@ def send_message(options = {})
 	to = options[:to] || DEFAULT_SEND_TO
   smtp = Net::SMTP.new 'smtp.gmail.com', 25
   smtp.enable_starttls
-  smtp.start("gmail.com", "mlvnotifier@gmail.com", '5Lipp7BDIk', :login) do |smtp|
-    smtp.send_message(full_message(to, options[:subject], options[:msg]), "mlvnotifier@gmail.com", to)
+  smtp.start("gmail.com", ENV['MAIL_LOGIN'], ENV['MAIL_PASSWORD'], :login) do |smtp|
+    smtp.send_message(full_message(to, options[:subject], options[:msg]), ENV['MAIL_LOGIN'], to)
   end
 end
