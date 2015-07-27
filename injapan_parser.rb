245a20434c8659eaf7fba254d99dbec2d77ec16a
Masilevich Leanid <l.masilevich@gmail.com>
2015-07-27 17:03:44 +0300

    remove sigma

--- injapan_parser.rb
+++ injapan_parser.rb
@@ -9,5 +9,4 @@ def parse_injapan(options)
 end
 
 parse_injapan(url: 'https://injapan.ru/search/do.html?query=S707SULT', product: 'S707SULT')
-parse_injapan(url: 'https://injapan.ru/search/do.html?query=Sigma+17-50+nikon', product: 'Sigma 17-50 Nikon', send_to: '4udo.v.kedax@gmail.com')
 parse_injapan(url: 'https://injapan.ru/search/do.html?query=PSSS-84T', product: 'Ever Green Salty Sensation PSSS-84T', send_to: 'banibaks89@mail.ru')
