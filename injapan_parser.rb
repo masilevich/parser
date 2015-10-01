82149a84a623b9f9b894aaeecaccac9b4bc3a0b5
Masilevich Leanid <l.masilevich@gmail.com>
2015-10-01 09:04:22 +0300

    remove salty sensation

--- injapan_parser.rb
+++ injapan_parser.rb
@@ -9,4 +9,3 @@ def parse_injapan(options)
 end
 
 parse_injapan(url: 'https://injapan.ru/search/do.html?query=S707SULT', product: 'S707SULT')
-parse_injapan(url: 'https://injapan.ru/search/do.html?query=PSSS-84T', product: 'Ever Green Salty Sensation PSSS-84T', send_to: 'banibaks89@mail.ru')
